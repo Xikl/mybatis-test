eee8d1b60fc091ae5a19c056e6f96a6e28b0d839
X1mo <1392936858@qq.com>
2017-09-29 12:47:17 +0800

    开始提交

---
+++ src/main/java/com/ximo/util/DBUtil.java
@@ -0,0 +1,30 @@
+package com.ximo.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * DB工具类
+ * Created by 朱文赵
+ * 2017/9/29
+ */
+public class DBUtil {
+
+    private static final String CONFIG_PATH = "Configuration.xml";
+
+    //获得sqlSession
+    public static SqlSession sqlSession() throws IOException {
+        //通过配置文件获取数据库连接信息
+        Reader reader = Resources.getResourceAsReader(CONFIG_PATH);
+        //通过配置信息构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        //打开SqlSession 并返回
+        return sqlSessionFactory.openSession();
+    }
+
+}
